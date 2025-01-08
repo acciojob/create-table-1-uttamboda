8be1852ba24e079ccd4295465917eea4a2b62f4d
uttamboda <167513333+uttamboda@users.noreply.github.com>
2025-01-08 13:30:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ function insert_Row() {
     //Write your code here
    var table = document.getElementById("sampleTable");
     
-    // Create a new row
     var newRow = table.insertRow(table.rows.length); 
 
     var cell1 = newRow.insertCell(0); 
     var cell2 = newRow.insertCell(1); 
     
-    // Add content to the new cells
-    cell1.innerHTML = "New Row cell1";
-    cell2.innerHTML = "New Row cell2";
+    cell1.innerHTML = "Row2 cell1";
+    cell2.innerHTML = "Row2 cell2";
   
 }
